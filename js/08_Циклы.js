77cf00e22d506ae3d703e484c7728aab9a5f334b
Godzilla <example@gmail.com>
2020-09-27 14:19:38 +0500

    прошли циклы

---
+++ js/08_Циклы.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// } 
+
+// do {
+//     console.log(num);
+//     num++;
+// } while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    if (i%2) {
+        // break;
+        continue;
+    }
+    console.log(i)
+}
+
