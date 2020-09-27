5d8ab09ec172f58d3d5251f6243489178b616836
Godzilla <example@gmail.com>
2020-09-27 11:39:25 +0500

    first commit

---
+++ js/02_Классификация типов данных.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// let number = 4;
+
+// console.log(-4/0)
+// console.log('string' * 9)
+
+// const person = `Alex`;
+
+// let und;
+// console.log(und);
+
+const obj = {
+    name: `John`,
+    age: 25,
+    isMArried: false
+}
+
+console.log(obj['name']);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+
+console.log(arr[1])
+document.write(arr[1])
---
+++ js/03_Простое общение с пользователем.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// alert('Hello')
+
+// const result = confirm('Are you here?');
+
+// document.write(result)
+
+// const answer = +prompt('Вам есть 18?', '')
+
+// document.write(answer + 5)
+
+const answers = [];
+
+answers[0] = prompt('Как вас зовут?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null));
