42d76128cdfb723846bd89a780f1617a7e03800e
Godzilla <example@gmail.com>
2020-10-02 16:44:54 +0500

    Прошли методы и свойства строк и чисел

---
+++ js/11_Методы и свойства строк и чисел.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// const str = "teSt";
+// const arr = [1, 2, 4]
+
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+// const fruit = "Some fruit";
+// console.log(fruit.indexOf("q"));
+
+// const logg = "Hello Worlds";
+
+// console.log(logg.slice(4, 11));
+
+// console.log(logg.substring(6, 11))
+
+// console.log(logg.substr(6, 3))
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
