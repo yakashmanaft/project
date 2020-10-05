52df3f3c33790799743066a6563e4dbfa313fca7
Godzilla <example@gmail.com>
2020-10-05 17:28:10 +0500

    прошли массивы и псевдомассивы

---
+++ js/15_массивы и псевдомассивы.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const arr = [1, 12, 33, 6, 8];
+
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+    return a - b;
+}
+
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr)
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// arr.pop();
+// console.log(arr);
+
+// arr.push(10);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value)
+// }
+
+// const str = prompt('', '');
+// const products = str.split(', ');
+// products.sort();
+// console.log(products.join('; '));
