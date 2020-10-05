974d4ec0fbf30e672939a0e7acfd909899f8a8ad
Godzilla <example@gmail.com>
2020-10-05 16:15:00 +0500

    ПРошли объекты и их деструктуризацию

---
+++ js/12_Объекты, деструктуризация объектов.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
+
+// options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+// console.log(options.colors.border)
+
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object' ) {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             // counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(counter);
+
+// console.log(Object.keys(options).length);
