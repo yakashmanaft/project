a993820de9e5f15b512208718f00517b1f01da4c
Godzilla <example@gmail.com>
2020-10-06 16:18:34 +0500

    Прошли ООП

---
+++ js/17_Основы ООП.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let str = "some";
+let strObj = new String(str);
+
+console.log(typeof(str));
+console.log(typeof(strObj));
+
+console.dir([1, 2, 3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('Hello');
+    }
+};
+
+const john = Object.create(soldier);
+
+// const john = {
+//     health: 100
+// };
+
+// john.__proto__ = soldier; //устаревший способ
+
+// Object.setPrototypeOf(john, soldier);
+
+
+// console.log(john.armor);
+john.sayHello();
+console.log(john.armor)
