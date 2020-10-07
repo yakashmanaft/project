d86c2dbe39e2289ae71722cc506936314e641fe0
Godzilla <example@gmail.com>
2020-10-07 14:58:12 +0500

    Прошли задачки по основам

---
+++ js/21_Задачи с собеседований на понимание основ.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// let x = 5;
+// alert( x++ ); //5
+
+// [] + false - null + true
+// console.log(typeof('' + false)); // "false"
+// console.log([] + false - null + true); //NaN
+
+// let y = 1; 
+// let x = y = 2; 
+// alert(x); // 2
+
+// console.log([] + 1 + 2); //12
+
+// alert("1"[0]); //1
+
+// console.log(2 && 1 && null && undefined); //null
+// && запинается на лжи, || всегда запинается на правде
+
+// console.log(!!(1 && 2) === (1 && 2))
+// !! превращает в булиновое выражение
+
+// alert(null || 2 && 3 || 4); //3
+
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+
+// console.log(a == b); // false
+
+// alert( +"infinity" );
+
+// console.log("Ёжик" > "яблоко"); //false
+
+// console.log(0 || "" || 2 || undefined || true || false); //2
