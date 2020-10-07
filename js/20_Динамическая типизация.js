119886e859938498ef47182fb17822912d8ea129
Godzilla <example@gmail.com>
2020-10-07 13:47:44 +0500

    Прошли динамическую типизацию

---
+++ js/20_Динамическая типизация.js
@@ -0,0 +1,59 @@
+'use strict';
+
+//to string;
+
+//1
+// console.log(typeof(String(null)));
+// console.log(typeof(String(4)));
+
+// //2
+// console.log(typeof(5 + ''));
+
+
+// const num = 5;
+// console.log("https://vk.com/catalog/" + num);
+
+// const fontSize = 26 + 'px';
+
+
+
+//to Number
+
+// //1
+// console.log(typeof(Number('4')));
+
+
+// //2
+// console.log(typeof(+'5'));
+
+// //3
+// console.log(typeof(parseInt('15px', 10)));
+
+
+// let answ = +prompt('Hello', '');
+
+
+
+// To boolean
+
+// 0, '', null, undefined, NaN - это False
+
+
+// 1
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+//2 
+console.log(typeof(Boolean('4')));
+
+//3
+console.log(typeof(!!"5453465"));
