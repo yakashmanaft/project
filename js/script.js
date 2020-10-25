ec4658870d299f6fbbb28a41fb53307c493a3e12
Godzilla <example@gmail.com>
2020-10-25 15:26:46 +0500

    Как сохранить данные без БД

--- js/script.js
+++ js/script.js
@@ -1,66 +1,45 @@
 'use strict';
 
-//filter ===============================================
+// localStorage.setItem('number', 5);
 
-// const names = ['Ivan', 'Ann', 'Sergey', 'Voldemart'];
+// localStorage.removeItem('number');
 
-// const shortNames = names.filter(function (name) {
-//     return name.length < 5;
-// });
+// localStorage.clear();
 
-// console.log(shortNames);
+// console.log(localStorage.getItem('number'));
 
-//map ==================================================
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color');
 
-// const answers = ['IvAn', 'AnnA', 'Hello'];
+if (localStorage.getItem('isChecked')) {
+    checkbox.checked = true;
+}
 
-// const result = answers.map((item) => item.toLowerCase());
+if (localStorage.getItem('bg') === 'changed') {
+    form.style.backgroundColor = 'red';
+}
 
-// console.log(result);
+checkbox.addEventListener('change', () => {
+    localStorage.setItem('isChecked', true);
+});
 
-//every/some ===========================================
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = '#fff';
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
 
-// Если some, то хотя бы один элемент массива должен соответствовать условию callback функции 
-// const some = [4, 'qwq', 'dfghjk'];
-// console.log(some.some(item => typeof(item) === 'number'));
-
-// Если every, то все элементы должны соответствовать условию callback-функции
-// console.log(some.every(item => typeof(item) === 'number'));
-
-//reduce ================================================
-
-//Собирает массив в одно целое
-
-// const arr = [1, 4, 5, 3, 2, 6];
-//         // sum = 0; current = 1;
-//         // sum = 1; current = 4;
-//         // sum = 5; current = 5;
-//         // sum = 10; current = 3;
-//         // sum = 13; current = 2;
-//         // sum = 15; current = 6;
-//         // sum = 21;
-
-
-// у reduce имеется третий аргумент - начальное значение
-
-// const res = arr.reduce((sum, current) => sum + current, 3);
-// console.log(res)
-
-// const arr = ['apple', 'pear', 'plum'];
-// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
-// console.log(res)
-
-// Объект entries ===========================================
-
-const obj = {
-    ivan: 'persone',
-    ann: 'persone',
-    dog: 'animal',
-    cat: 'animal'
+const presone = {
+    name: 'Alex',
+    age: 25
 };
 
-const newArr = Object.entries(obj)
-.filter(item => item[1] === 'persone')
-.map(item => item[0])
+const serializedPersone = JSON.stringify(presone);
+localStorage.setItem('alex', serializedPersone);
 
-console.log(newArr)
+console.log(JSON.parse(localStorage.getItem('alex')))
