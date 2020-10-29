f4753597b46b006b64057e15648af006d3be914b
Godzilla <example@gmail.com>
2020-10-29 19:47:18 +0500

    геттеры и сеттеры

---
+++ js/03_Продвинутый JavaScript/06_Геттеры и сеттеры.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const person = {
+    name: 'Alex',
+    age: 25,
+
+    get userAge() {
+        return this.age;
+    },
+
+    set userAge(num) {
+        this.age = num;
+    }
+};
+
+console.log(person.userAge = 30);
